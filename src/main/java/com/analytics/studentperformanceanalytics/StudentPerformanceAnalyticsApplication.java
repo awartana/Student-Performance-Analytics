b32e95631c7114050df4976149cb4e35855a33e2
Awartana Prawin <awartanap@gmail.com>
2025-07-18 00:41:40 +0530

    Initial commit

---
+++ src/main/java/com/analytics/studentperformanceanalytics/StudentPerformanceAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.analytics.studentperformanceanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPerformanceAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentPerformanceAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/analytics/studentperformanceanalytics/StudentPerformanceAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.analytics.studentperformanceanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPerformanceAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
